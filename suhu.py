9a2e2cbb980c37ac2c7ea9372a13f8791df74f8b
01consigliere <anan.realmeXT@gmail.com>
2024-09-23 10:23:30 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -17,7 +17,33 @@ if(sx=="C"):
         y = (9*x/5) + 32
     elif(sy=="K"):
         y = x + 273
-
+if(sx=="F"):
+    if(sy=="F"):
+        y = x
+    elif(sy=="C"):
+        y = (x - 32)*5/9
+    elif(sy=="R"):
+        y = (x - 32)*4/9
+    elif(sy=="C"):
+        y = ((x - 32)*5/9) + 273
+if(sx=="R"):
+    if(sy=="R"):
+        y = x
+    elif(sy=="C"):
+        y = (x)*5/4
+    elif(sy=="F"):
+        y = (x - 32)*9/4
+    elif(sy=="C"):
+        y = (x*5/4) + 273
+if(sx=="K"):
+    if(sy=="K"):
+        y = x
+    elif(sy=="R"):
+        y = (x - 273)*4/5
+    elif(sy=="F"):
+        y = ((9/5)*(x-273))+32
+    elif(sy=="C"):
+        y = x - 273
 
 
 
